6067ca6ea30313805945bed1d0d0011eb580143c
MarcBehnke <marc.behnke@gmail.com>
2018-02-19 21:05:51 -0600

    Created a visual studio solution and .net core console project.

---
+++ src/TheBrainTextParser/TheBrainTextParser/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TheBrainTextParser
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
